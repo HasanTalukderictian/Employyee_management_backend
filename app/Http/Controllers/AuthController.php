a7631c66a88a0948f3b11ffd6d5901a6d2ab741a
HasanTalukderIctian <hasantalukdercou@gmail.com>
2026-04-22 15:44:07 +0600

    Employee update

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
